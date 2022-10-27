e45ff83fa89442882a37868bad9c8aa4d8104fce
Madhav Garg <c2064676@newcastle.ac.uk>
2022-10-27 14:29:14 +0100

    Add title

--- main_bank.py
+++ main_bank.py
@@ -2,10 +2,10 @@ import bank
 
 
 def main():
-    madhav = bank.Client('Madhav', 'Garg', 'He/Him', '11/01/2003', 'Student', 1000, 100)
-    gare = bank.Client('Gare', 'Coffelt', 'He/Him', '24/05/2006', 'Student', 25463, 100)
-    ab = bank.Client('Ab', 'Suter', 'They/Them', '26/05/1986', 'Marketing Assistant', 58670, 100)
-
+    madhav = bank.Client('Mr.', 'Madhav', 'Garg', 'He/Him', ' ', 'Student', 1000, 100)
+    gare = bank.Client('Mr.', 'Gare', 'Coffelt', 'He/Him', '24/05/2006', 'Student', 25463, 100)
+    ab = bank.Client('Mrs.', 'Ab', 'Suter', 'She/Her', '26/05/1986', 'Marketing Assistant', 58670, 100)
+    x = input("what do you want to do?")
     print(gare.show_details())
     print(ab.show_details())
     madhav.update_first_name('Madhav1')
